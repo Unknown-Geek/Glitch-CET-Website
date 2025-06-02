f6be5b618859e50ef0cbe24e8cd289cc90d65482
Robin-github-123 <151839372+Robin-github-123@users.noreply.github.com>
2025-06-03 00:01:24 +0530

    Routes setup and games page

--- frontend/src/Navbar/Navbar.jsx
+++ frontend/src/Navbar/Navbar.jsx
@@ -1,31 +1,40 @@
 import React from 'react';
-import './Navbar.css'
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
+  const navItems = [
+    { name: 'Home', path: '/' },
+    { name: 'Games', path: '/games' },
+    { name: 'Events', path: '/events' },
+    { name: 'Team', path: '/team' },
+    { name: 'Contact', path: '/contacts' },
+  ];
+
   return (
     <div className="flex justify-center mt-[30px]">
-      <div className="navbar-container flex items-center justify-between text-white rounded-[15px] border border-[#F92672] px-[14px] py-[10px] mx-auto  ">
+      <div className="navbar-container flex items-center justify-between text-white rounded-[15px] border border-[#F92672] px-[14px] py-[10px] mx-auto">
         <div>
           <img
             className="logo-img p-[14px]"
             src="glitch_font.png"
-            alt="logo"
+            alt="Glitch Logo"
           />
         </div>
 
-        <div className="navbar-links flex items-center">
-          {['Home', 'Games', 'Events', 'Team', 'Contact'].map((item, index) => (
-            <span
+        <div className="navbar-links flex items-center space-x-6">
+          {navItems.map((item, index) => (
+            <Link
               key={index}
+              to={item.path}
               style={{ fontFamily: 'neopixel' }}
-              className="navbar-text font-light cursor-pointer hover:text-[#ED246D]"
+              className="navbar-text font-light cursor-pointer hover:text-[#ED246D] transition-colors duration-200"
             >
-              {item}
-            </span>
+              {item.name}
+            </Link>
           ))}
         </div>
       </div>
-      
     </div>
   );
 }
