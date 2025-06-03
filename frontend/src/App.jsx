d47f4d181a56b21fb654e12554dbba986d90e85f
KK-8848 <karthikkrishna8848@gmail.com>
2025-06-03 14:46:16 +0530

    About section initial structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,42 @@
-import { useState } from 'react'
-import Navbar from './Navbar/Navbar'
-import './styles.css'
-import Start from './Start/Start'
-import './index.css' // Importing the CSS file for styles
-import Featured from '../pages/Home/Featured.jsx' 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Cards from '../pages/Games/Cards.jsx'
-
+import { useState } from "react";
+import Navbar from "./Navbar/Navbar";
+import "./styles.css";
+import Start from "./Start/Start";
+import "./index.css"; // Importing the CSS file for styles
+import Featured from "../pages/Home/Featured.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cards from "../pages/Games/Cards.jsx";
+import About from "../pages/Home/About.jsx";
 function App() {
-
   return (
-    
     <>
       <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Navbar />
+                <Start />
+                <About />
+                <Featured />
+              </>
+            }
+          />
 
-      <Routes>
-        <Route
-          path="/"
-          element=
-          {
-          <>
-            <Navbar />
-            <Start />
-            <Featured />
-          </>
-          }
-        />
-
-        <Route
-          path="/games"
-          element=
-          {
-          <>
-            <Navbar />
-            <Cards/>
-          </>
-          }
-        />
-       
-      </Routes>
-
-    </BrowserRouter>
-      
-    </>)
+          <Route
+            path="/games"
+            element={
+              <>
+                <Navbar />
+                <Cards />
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
