81b54248d78a6cf33fa61c9aa5a9aefd30ed7218
Parikshit <parikshit.r2005@gmail.com>
2025-06-05 09:48:54 +0530

    added scrolling effect

--- frontend/pages/Home/ScrollingText.jsx
+++ frontend/pages/Home/ScrollingText.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 
 function ScrollingText({text}) {
+    const [direction, setDirection] = useState('right');
+    const lastScrollDirection = useRef(null);
+    useEffect(() => {
+        const handleScroll = (e) => {
+            const currentDirection = e.deltaY > 0 ? 'down' : 'up';
+
+            if (lastScrollDirection.current && lastScrollDirection.current !== currentDirection) {
+                setDirection((prev) => (prev === 'left' ? 'right' : 'left'));
+            }
+            lastScrollDirection.current = currentDirection;
+        };
+        window.addEventListener('wheel', handleScroll);
+        return () => window.removeEventListener('wheel', handleScroll);
+    }, []);
     return (
         <div className="overflow-hidden border-t-7 border-b-7 border-[#D4D29C] h-[130px] sm:h-[150px] md:h-[180px] lg:h-[225px] flex items-center">
             <style>
         {`
-          @keyframes scroll-right {
-            0% { transform: translateX(-30%); }
-            100% { transform: translateX(0); }
-          }
-          .scroll-animation {
-          display:inline-flex;        
-          animation: scroll-right 20s linear infinite;
-          }
+            @keyframes scroll-right {
+            0%{transform: translateX(-30%);}
+            100%{transform: translateX(0);}
+            }
+            @keyframes scroll-left {
+            0%{transform: translateX(0);}
+            100%{transform: translateX(-30%);}
+            }
+            .scroll-right{
+                animation: scroll-right 20s linear infinite;
+            }
+            .scroll-left{
+                animation: scroll-left 20s linear infinite;
+            }
         `}
             </style>
-            <div className="flex scroll-animation w-fit">
+            <div className={`flex w-fit ${direction === 'right' ? 'scroll-right' : 'scroll-left'}`}>
                 {[...Array(10)].map((_, i) => (
                 <React.Fragment key={i}>
                     <span style={{ fontFamily: "Morton", color: "transparent", WebkitTextStroke: "1px white", }} className='py-[10px] text-[65px] sm:text-[100px] md:text-[130px] lg:text-[200px]'>
