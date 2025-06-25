2f185cc8dabf3aed5e4a1b3e8ac5b269f861401d
Parikshit <parikshit.r2005@gmail.com>
2025-06-25 14:42:19 +0530

    previous events infinite scroll

--- frontend/pages/Events/PreviousEvents.jsx
+++ frontend/pages/Events/PreviousEvents.jsx
@@ -43,16 +43,16 @@ function PreviousEvents() {
                             : (clicked ? 15 : 50);
                         {/* Previous event card */ }
                         return (
-                            <div onClick={() => toggleCard(id)} className={`relative z-10 border-[1px] rounded-[3px] flex flex-col items-center justify-center py-[20px] cursor-pointer mx-[20px] ${clicked ? "w-[450px] lg:w-[800px] " : "w-[320px]  lg:w-[340px] "}  `}>
+                            <div onClick={() => toggleCard(id)} className={`relative overflow-hidden z-10 border-[1px] rounded-[3px] flex flex-col items-center justify-center py-[5px] lg:py-[20px] cursor-pointer mx-[20px] ${clicked ? "w-[450px] lg:w-[800px] " : "w-[290px]  lg:w-[340px] "}  `}>
 
 
                                 {/* background image */}
-                                <img className={`absolute z-0 pointer-events-none opacity-[0.2] brightness-[1.6] ${clicked ? "h-[205%] w-[125%] lg:w-[105%] " : "h-[95%]"} `} src="DotsBG.png" alt="" />
+                                <img className={`absolute z-0 pointer-events-none opacity-[0.2] brightness-[1.6] ${clicked ? "h-[205%] w-[125%] lg:w-[105%] " : "h-[95%] w-[110%] "} `} src="DotsBG.png" alt="" />
 
                                 {/* title */}
                                 <div className="flex flex-row items-center justify-center px-[10px] lg:px-[20px] ">
 
-                                    <div className="mt-[20px]  flex flex-col gap-[3px] ">
+                                    <div className="mt-[10px]  flex flex-col gap-[3px] ">
                                         <div className="flex space-x-0.5 flex-col gap-[3px]">
                                             {Array.from({ length: isMobile ? "8" : "12" }, (_, i) => i).map((_, index) => (
                                                 <div
@@ -64,24 +64,24 @@ function PreviousEvents() {
                                         </div>
 
                                     </div>
-                                    <span className={`leading-none px-[10px] ${clicked ? "text-[50px] lg:text-[70px] " : "text-[35px]"} `} style={{ fontFamily: "Neopixel" }}>{title}</span>
+                                    <span className={`leading-none px-[6px] ${clicked ? "text-[40px] lg:text-[70px] " : "text-[35px]"} `} style={{ fontFamily: "Neopixel" }}>{title}</span>
                                 </div>
                                 <span className={`text-center my-[20px] font-light ${clicked ? "text-2xl lg:text-3xl " : "text-xl "} `} style={{ fontFamily: "Neopixel" }}>{subTitle} </span>
 
                                 {/* event image and description */}
                                 <div className="flex flex-row mt-[30px] lg:mt-[60px] justify-center items-center ">
-                                    <img className={`w-[170px] lg:w-[250px] px-[10px] lg:px-0  ${clicked ? "h-[50%] lg:h-[90%] " : ""} `} src='event.png' alt="event" />
+                                    <img className={`w-[170px] lg:w-[250px] px-[5px] lg:px-0  ${clicked ? "h-[50%] lg:h-[90%] " : ""} `} src='event.png' alt="event" />
                                     {clicked && (
-                                        <div className="ml-[5px] lg:ml-[15px] ">
-                                            <span style={{ fontFamily: "Pixelify Sans" }} className="flex justify-center items-center w-[250px] lg:w-[370px] pl-[30px] text-[15px] lg:text-xl ">{description1}</span>
-                                            <span style={{ fontFamily: "Pixelify Sans" }} className="flex justify-center items-center w-[250px] lg:w-[370px] pl-[30px] text-[15px] lg:text-xl mt-[25px] ">{description2}</span>
+                                        <div className="ml-[0px] lg:ml-[15px] ">
+                                            <span style={{ fontFamily: "Pixelify Sans" }} className="flex justify-center items-center w-[230px] lg:w-[370px] pl-[30px] text-[15px] lg:text-xl ">{description1}</span>
+                                            <span style={{ fontFamily: "Pixelify Sans" }} className="flex justify-center items-center w-[230px] lg:w-[370px] pl-[30px] text-[15px] lg:text-xl mt-[25px] ">{description2}</span>
                                         </div>
                                     )}
                                 </div>
 
-                                <div className={`${clicked ? "flex flex-row mt-[50px] px-[10px] lg:px-[20px] " : "px-[10px] sm:px-[5px] md:px-[10px] lg:px-[20px] "}`}>
+                                <div className={`${clicked ? "flex flex-row mt-[40px] px-[10px] lg:px-[20px] " : "px-[10px] sm:px-[5px] md:px-[10px] lg:px-[20px] "}`}>
                                     {/* lineBar */}
-                                    <div className={` mt-[20px] ${clicked ? "flex flex-col" : "flex flex-row"} `}>
+                                    <div className={` my-[10px] ${clicked ? "flex flex-col" : "flex flex-row"} `}>
                                         <div className={`flex space-x-0.5 ${clicked ? "flex-col gap-[3px] " : "flex-row"} `}>
                                             {Array.from({ length: barCount }, (_, i) => i).map((_, index) => (
                                                 <div
@@ -96,7 +96,7 @@ function PreviousEvents() {
 
                                     {/* Date */}
                                     <div className={`text-center mt-[5px] ${clicked ? "flex flex-row text-center px-[10px] " : ""} `}>
-                                        <span style={{ fontFamily: "Morton" }} className={` ${clicked ? "text-4xl lg:text-7xl mt-[20px] lg:mt-[0px] " : "text-6xl lg:text-7xl"} `}>{day}</span>
+                                        <span style={{ fontFamily: "Morton" }} className={` ${clicked ? "text-4xl lg:text-7xl mt-[15px] lg:mt-[0px] " : "text-6xl lg:text-7xl"} `}>{day}</span>
                                         <div className={` ${clicked ? "flex justify-center items-center ml-[8px] lg:ml-[16px] " : ""} `}>
                                             <span style={{ fontFamily: "Morton" }} className={`text-2xl text-[#ED246D] px-[8px] ${clicked ? "text-3xl lg:text-5xl " : ""} `}>{month}</span>
                                             <span style={{ fontFamily: "Morton" }} className={`text-2xl leading-none ${clicked ? "text-3xl lg:text-5xl ml-[6px] lg:ml-[12px] mr-[10px] " : ""} `}>{clicked ? "2025" : "25"} </span>
@@ -104,8 +104,8 @@ function PreviousEvents() {
                                     </div>
 
                                     {/* lineBar */}
-                                    <div className="flex flex-col mt-[20px] ">
-                                        <div className={`flex space-x-0.5 ${clicked ? "flex-col gap-[3px] " : "flex-row"} `}>
+                                    <div className="flex flex-col my-[10px] ">
+                                        <div className={`flex  space-x-0.5 ${clicked ? "flex-col gap-[3px] " : "flex-row"} `}>
                                             {Array.from({ length: barCount }, (_, i) => i).map((_, index) => (
                                                 <div
                                                     key={index}
