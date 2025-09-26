ef11bcff629f3200617b98220a0345ca93a730d8
Robin-github-123 <151839372+Robin-github-123@users.noreply.github.com>
2025-09-27 00:35:57 +0530

    changes

--- frontend/pages/Home/Footer.jsx
+++ frontend/pages/Home/Footer.jsx
@@ -3,21 +3,21 @@ import image from "../../public/glitch.png"
 
 export default function GlitchBanner() {
   return (
-    <div className="hidden md:flex flex-col gap-2 ml-6 mr-6 ">
-        <div className="mt-12  ml-12 flex flex-row gap-64 font-pixelify ">
-            <p >
+    <div className=" flex-col gap-2 ml-6 mr-6 ">
+        <div className="md:mt-12 mt-4  md:ml-12 flex flex-row md:gap-64 font-pixelify ">
+            <p className="hidden md:flex" >
                 2025 GLITCH CET • ALL RIGHTS RESERVED
             </p>
-            <p className="ml-12">
+            <p className="hidden md:flex ml-12 ">
                 THE GAME DEVELOPMENT CLUB OF CET
             </p>
-            <p className="flex flex-row gap-6 ml-10">
+            <p className="flex flex-row md:gap-6 gap-10 md:ml-10 ml-5 md:mb-0 mb-2">
                 <span>□  INSTAGRAM</span>
                 <span>□  LINKEDIN</span>
                 <span>□  Discord</span>
             </p>
         </div>
-        <img className="w-[1480px] h-[220px] ml-12 mb-10 " alt="Glitch Logo"
+        <img className="md:w-[1480px] md:h-[220px] md:ml-12 mb-10 " alt="Glitch Logo"
         src={image}
         />
     </div>
